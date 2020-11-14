d77e639d480d0f7533a5b04eb0b43450daf0f0c6
your_username <mehdi.makni@polytechnique.edu>
2020-11-14 20:30:09 +0100

    first sketch of stock and api class

--- src/api/api.cpp
+++ src/api/api.cpp
@@ -2,15 +2,57 @@
 
 // Constructor of the API class
 API::API() {
-	// Initiate the CURL object here
+    // Initiate the CURL object
+    curl_global_init(CURL_GLOBAL_DEFAULT) ;
+
 }
 
-// Deconstructor of the API class
 API::~API() {
-	// Delete the CURL object here
+    curl=NULL;
 }
-
+size_t writefunc(void *ptr, size_t size, size_t nmemb, std::string *s) {
+  s->append(static_cast<char *>(ptr), size*nmemb);
+  return size*nmemb;
+}
+void API::url_set(std::string stockSymbol,std::string resolution, long startTimestamp, long endTimestamp){
+        url_used=url+stockSymbol+"&resolution="+resolution+"&from="+std::to_string(startTimestamp)+"&to"+std::to_string(endTimestamp);
+    }
 // Gets the stock from the API
-std::map<std::string, double> API::getStockData(std::string stockSymbol, long startTimestamp, long endTimestamp) {
-	return {};
-}
+std::string API::getStockData(std::string stockSymbol,std::string resolution, long startTimestamp, long endTimestamp) {
+    //set the URL parameters
+    url_set( stockSymbol,resolution, startTimestamp,  endTimestamp);
+    curl=curl_easy_init();
+    // Url settings
+
+
+    if (curl){
+        long httpCode(0);//Initialize the http code to 0, it can't be an int it must be a long int
+        // Website settings
+        curl_easy_setopt(curl, CURLOPT_URL,url_used.c_str());
+        std::string token_header = "X-Finnhub-Token:" + token; // token is our API key
+        // Add the token parameter to the header
+        headers = curl_slist_append(headers, token_header.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        //Follow redirections
+        curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+        // Set the request mode to GET
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
+        //Handle the data container
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+        curl_easy_setopt(curl,CURLOPT_WRITEDATA,&data);
+        //Perform the GET request and store the HTTP code
+        result=curl_easy_perform(curl);
+        // always cleanup
+        curl_easy_cleanup(curl);
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &httpCode);
+        // 200 means successful transfer
+        if (httpCode==200 && result==CURLE_OK ){
+            //  contains the requested data
+            //json j= json::parse(data);
+            //Return the data
+            return data;
+                }
+            }
+    return NULL;
+    }
+
--- src/api/api.h
+++ src/api/api.h
@@ -1,18 +1,28 @@
 #ifndef API_H
 #define API_H
 
-#include <iostream>
 #include <map>
-
+#include <string>
+#include <curl/curl.h>
+//#include <nlohmann/json.hpp>
+//using json = nlohmann::json;
 class API {
 public:
-	API();
-	~API();
-
-	std::map <std::string, double> getStockData(std::string stockSymbol, long startTimestamp, long endTimestamp);
-
+    API();
+    ~API();
+    void url_set(std::string stockSymbol,std::string resolution, long startTimestamp, long endTimestamp) ;// Contains the url + parameters
+    std::string getStockData(std::string stockSymbol,std::string resolution, long startTimestamp, long endTimestamp);
+    std::string get(){ return url_used;};
 private:
-	// CURL object here
+    // CURL object here
+    CURL *curl ;
+    CURLcode result;
+    struct curl_slist *headers= NULL;
+    std::string token = "buk0iff48v6r2017bpg0"; // The API token which will be used
+    std::string data; //Contains the data
+    std::string url="https://finnhub.io/api/v1/stock/candle?symbol=";
+    std::string url_used;
 };
+size_t writefunc(void *ptr, size_t size, size_t nmemb, std::string *s); // Writing function for curl request
 
 #endif // API_H
