719c659ad564824551cf23a548073e179307aac2
Khanh Nguyen <ngkan146@gmail.com>
2020-11-16 15:32:08 +0100

    Update run.sh
    Current run.sh file only supports Linux OS. I added a few lines so it can
    be run on both Linux and Windows.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-./build/trading-platform-pp
+# Windows
+if [[ ("$OSTYPE" == "cygwin") ||  ("$OSTYPE" == "msys")]]; then 
+    ./build/release/trading-platform-pp
+# Linux
+else
+    ./build/trading-platform-pp 
+fi
