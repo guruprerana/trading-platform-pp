f0a40a619eff97aa069732c93c04f418bafc2a02
maayaneattias <maayane-lea.attias@polytechnique.edu>
2020-12-17 13:09:16 +0100

    Fixed code
    I fixed the errors of the strategy class and modified the functions so they fit with the overall architecture of the class. The simulate function remains remains to be added by Frederic.

---
+++ src/recommendation/strategy.cpp
@@ -0,0 +1,225 @@
+#include "strategy.h"
+#include "../components/stock.h"
+#include "../helper/QJsonObjectManipulation.h"
+#include <string>
+#include <tuple>
+#include <map>
+#include <cmath>
+using std::string;
+using std::map;
+
+Strategy::Strategy(){
+    data_by_minute = true;
+    this->strategy_name = "None";
+    this->stock = nullptr;
+    this->data_by_minute = false; //by default it is by day 
+    this->price_type = 'c'; // by default consider closing price 
+}
+
+Strategy::Strategy(char& strategy_name, Stock *stock, bool data_by_minute, std::string price_type){
+    this->strategy_name = strategy_name; //possible names {'MOM', 'EMA', 'LR'}
+    this->stock = stock;
+    this->data_by_minute = data_by_minute; //if false, the data is provided by day
+    this->price_type = price_type;
+}
+
+Strategy::~Strategy(){
+    delete stock;
+}
+
+
+
+//Return only a map {time:price} for the last n days of starting from k days ago (k=0 for today, k=1 for yesterday...)
+std::map<long, double> Strategy::get_data(int N, int){
+    if (this->data_by_minute){
+        this->stock->updateDataByMinute();
+        QJsonObject jsonData = this->stock->getDataByMinute();
+    }
+    else{
+        this->stock->updateDataByDay();
+        QJsonObject jsonData = this->stock->getDataByDay();
+    }
+    std::map<std::string, std::map<long, double>> mapData = convertToMap(jsonData); //converts the QJsonObject with the data of the last 6 months into a std::map<std::string, std::ap<long, double>> {price_type : {time:price}}
+    for (auto it = mapData.begin(); it != mapData.end(); ++it){
+        if (it->first == this->get_price_type()){ //take the map <long, double> associated to the price_type chosen at the beginning
+            std::map<long, double> price_map = it->second;
+        }
+    }
+    std::map<long, double> res;
+    int count = 0;
+    for (auto it = price_map.begin(); it != price_map.end(); ++it){
+        if (count >=k && count <N+k){ 
+            res[it->first] = it->second;
+        }
+        if (count==N+k){
+           return res;
+        }
+        count += 1;
+    }
+}
+
+bool Strategy::exponential_moving_average(){
+
+    std::map<long, double> bars_11 = this->get_data(11);
+    std::map<long, double> bars_6 = this->get_data(6);
+    double ema_11 = this->calculate_ema(bars_11); // Longer Moving Average
+    double ema_06 = this->calculate_ema(bars_6);  // Shorter Moving Average
+    double markup = 0.05;// markup introduces anticipation into our strategy:
+    if (ema_06 > (1-markup)*ema_11 ) {
+        return true;
+    }
+    else if (ema_11 > (1+markup)*ema_06 ) {
+        return false;
+    }
+}
+
+
+// Calculate Simple Moving Average 
+double Strategy::calculate_sma(std::map<long, double> &bars) {
+         assert(!bars.empty());
+         double sum = 0;
+         for (auto &it : bars) {
+             sum += it.second;
+         }
+         return sum / bars.size();
+     }
+
+
+// Calculate Exponential Moving Average
+double Strategy::calculate_ema(std::map<long, double> &bars) {
+         assert(!bars.empty());
+         double e_MovingAverage = 0;
+         double smoothing_parameter = 0.4; //decay factor of terms in Moving Average
+         int weight_factor = 0; // degree of the smoothing_parameter for a given summand
+         for (auto &it : bars) {
+             e_MovingAverage += smoothing_parameter * (std::pow((1-smoothing_parameter),weight_factor)) * it.second;
+             weight_factor += 1;
+         }
+         return e_MovingAverage;
+     }
+
+
+int Strategy::auxiliary_momentum(std::map<int, double> &cache){
+ //1-full, 0-sell, -1-perc
+     if(cache.at(0)<1)
+         return 0;
+     double diff1=cache.at(3)-cache.at(2);
+     double diff2=cache.at(1)-cache.at(0);
+
+     if (diff1<=diff2)
+         return 1;
+     return -1;}
+    
+std::tuple<bool, double> Strategy::momentum(){
+     std::map<int, double> cache;
+     for (int k=0; k<4; k++){
+         std::map<long, double> bars_10 = this->get_data(9,k);
+         std::map<long, double> bars_5 = this->get_data(4,k);
+         double sma_10 = this->calculate_sma(bars_10);
+         double sma_5 = this->calculate_sma(bars_5);
+         double moment = sma_5/sma_10;
+         cache.insert(std::pair<int, double>(k, moment));
+     }
+
+     int action = this->auxiliary_momentum(cache);
+     if (action == 0 || action == 1){
+         return std::make_tuple(true, 1.0);
+     }
+     else if (action == -1) {
+         return std::make_tuple(true, 0.8);
+     }      
+
+}
+
+double Strategy::compute_average_key(std::map<long, double> &bars){
+    double key_average = 0;
+    for (auto &item : bars) {
+            key_average += item.first;
+        }
+    return (key_average/bars.size());
+}
+
+double Strategy::compute_average_value(std::map<long, double> &bars){
+    double value_average = 0;
+    for (auto &item : bars) {
+            value_average += item.second;
+        }
+    return (value_average/bars.size());
+}
+
+std::tuple<bool, double> Strategy::auxiliary_linear_regression(std::map<long, double> &bars){
+    assert(!bars.empty());
+    double value_average= this->compute_average_value(bars);
+    double key_average= this->compute_average_key(bars);
+    double ss_xy = 0;
+    double ss_xx = 0;
+    for (auto &item : bars) {
+            ss_xx += (item.first- key_average)* (item.second - value_average);
+            ss_xy += pow(item.first- key_average, 2);
+        }
+    double slope = ss_xx/ss_xy;
+    double intercept = value_average -slope* key_average;
+    return std::make_tuple(slope, intercept); //use the slope and the y-intercept in the plot of simulate
+
+}
+
+bool Strategy::linear_regression(){
+    std::map<long, double> bars = this->get_data(20);
+    auto res = this->auxiliary_linear_regression(bars);
+    double slope = std::get<0>(res);
+    if (slope <= 0){ 
+        return false;
+    }
+    else{
+        return true; 
+    } 
+}
+
+std::tuple<bool, double> Strategy::calculate_signals(){
+    bool bought;
+    double percentage = 1.0;
+    //provides outcome of the chosen strategy
+    std::string str1 ("EMA");
+    std::string str2 ("MOM");
+    std::string str3 ("LR");
+    std::string name = this->get_name();
+    if (str1.compare(name) == 0){ //exponential moving average
+        bought =  this->exponential_moving_average();
+     }
+    else if (str2.compare(name) == 0){
+        auto res = this->momentum();
+        bought = std::get<0>(res);
+        percentage = std::get<1>(res);
+    }
+    if (str3.compare(name) == 0){//linear regression
+        bought= this->linear_regression();
+    } 
+    return std::make_tuple(bought, percentage);
+}
+
+void Strategy::simulate(){
+
+}
+
+void Strategy::evaluate(){
+
+}
+void Strategy::compare(Strategy* other_strategy){
+
+}
+
+
+void Strategy::set_stock(Stock* stock){
+    this->stock = stock;
+}
+Stock* Strategy::get_stock(){
+    return this->stock;
+
+}
+std::string Strategy::get_name(){
+    return  this->strategy_name;
+}
+std::string Strategy::get_price_type(){
+    return  this->price_type;
+}
+
---
+++ src/recommendation/strategy.h
@@ -0,0 +1,59 @@
+#ifndef STRATEGY_H
+#define STRATEGY_H
+
+#include "../components/stock.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
+using std::string;
+using std::map;
+
+class Strategy
+{
+public:
+    Strategy();
+    Strategy(char& strategy_name, Stock *stock, bool data_by_minute, std::string price_type);
+    ~Strategy();
+    void set_stock(Stock* stock);
+    Stock* get_stock();
+    std::string get_name();
+    std::string get_price_type();    
+    std::map<long, double> get_data(int N, int k =0);
+    double calculate_sma(std::map<long, double> &bars);
+    double calculate_ema(std::map<long, double> &bars);
+    int auxiliary_momentum(std::map<int, double> &cache);
+    bool exponential_moving_average();
+    double compute_average_value(std::map<long, double> &bars);
+    double compute_average_key(std::map<long, double> &bars);
+    std::tuple<bool, double> auxiliary_linear_regression(std::map<long, double> &bars);
+    bool linear_regression();
+    std::tuple<bool, double> momentum();
+
+
+protected:
+    std::tuple<bool, double> calculate_signals();
+    // The heart of each strategy, which we have already implemented within subteams
+
+    /* virtual */ void simulate();
+
+    // Plot the graphs associated with the strategy
+    // Add BUY/SELL signal points
+
+
+
+    // more advanced member functions - to be implemented if time permits
+
+    void evaluate(); // Report technical assessment on strategy's performance
+    void compare(Strategy* other_strategy); // compare strategies with criteria list
+
+    // Data members
+    std::string strategy_name;
+    Stock* stock; // stock under consideration
+    bool data_by_minute;
+    std::string price_type;// price_type is a lower case string in ['o','c','h','l','v']
+
+
+};
+
+#endif // STRATEGY_H
