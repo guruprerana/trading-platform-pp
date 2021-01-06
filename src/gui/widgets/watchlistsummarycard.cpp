270905030e7882444756bdd40ed01fecd89df1dc
MaximeLamy <72193530+MaximeLamy@users.noreply.github.com>
2021-01-06 16:58:58 +0000

    Connected watchlist widget display to stock data to show ticker, value, and daily change

--- src/gui/widgets/watchlistsummarycard.cpp
+++ src/gui/widgets/watchlistsummarycard.cpp
@@ -7,6 +7,17 @@ WatchlistSummaryCard::WatchlistSummaryCard(Stock *stock, QWidget *parent) :
   ui->setupUi(this);
 
   this->stock = stock;
+
+  stock->getDataByDay();
+
+  std::string ticker = stock->getSymbol();
+  QMap<std::string, QVector<double>> valuation = stock->getDataByMinute();
+  double delta = 100 * (stock->getDataByDay()["c"].toArray().last().toDouble()) / stock->getDataByDay()["c"].toArray().last().toDouble();
+
+  ui->valuation->setText(QString::number(valuation["c"].last()));
+  ui->ticker->setText(QString::fromStdString(ticker));
+  ui->delta->setText(QString::number(delta));
+
 }
 
 WatchlistSummaryCard::~WatchlistSummaryCard() {
